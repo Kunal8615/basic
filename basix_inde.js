307e46a41c7d4b64bcc89be473e3f6fa7c199d5f
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-05-11 18:58:56 +0530

    git saved data

---
+++ basix_inde.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+//console.log(process.env) // remove this after you've confirmed it is working
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('kunal8615')
+  })
+
+  app.get('/text', (req, res) => {
+    res.send('<h1> bold test here</h1>')
+  })
+//ACCESS PRT BY ENV
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- index.js
+++ index.js
@@ -3,17 +3,24 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
-
+const data ={
+  kunal: "demo"
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//hellowdasd
 app.get('/twitter', (req, res) => {
     res.send('kunal8615')
   })
 
   app.get('/text', (req, res) => {
     res.send('<h1> bold test here</h1>')
+  })
+
+  app.get("/data",(req,res)=>{
+    res.json(data)
   })
 //ACCESS PRT BY ENV
 app.listen(process.env.port, () => {
